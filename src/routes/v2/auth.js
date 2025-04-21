9b4dd98097da8f2b18fa836fe0c41bca28f5d5c7
Richard Alhama <richard.alhama@gmail.com>
2025-04-21 15:49:30 +0800

    no register route on v2 in production

--- src/routes/v2/auth.js
+++ src/routes/v2/auth.js
@@ -5,21 +5,23 @@ const authController = require('../../controllers/v2/authController');
 const { body, validationResult } = require('express-validator');
 
 router.post('/login', authController.login);
-router.post(
-  '/register',
-  [
-    body('username').isLength({ min: 3 }).trim().escape(),
-    body('email').isEmail().normalizeEmail(),
-    body('password').isLength({ min: 6 })
-  ],
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    next();
-  },
-  authController.register);
+if (process.env.NODE_ENV !== 'production') {
+  router.post(
+    '/register',
+    [
+      body('username').isLength({ min: 3 }).trim().escape(),
+      body('email').isEmail().normalizeEmail(),
+      body('password').isLength({ min: 6 })
+    ],
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      next();
+    },
+    authController.register);
+}
 
 module.exports = router;
 
